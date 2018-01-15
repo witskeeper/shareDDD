b02389aae1ef927424640fd2eb1e5a1e2ddcfc90
wanglijuan <lijuan.wang@xiaobaoonline.com>
2018-01-15 19:19:59 +0800

    edit

---
+++ src/main/web/src/views/environment/environment-configuration.vue
@@ -0,0 +1,94 @@
+<style lang="less">
+    @import './environment-configuration.less';
+</style>
+
+<template>
+    <div>
+        <Row>
+            <Card>
+                <p slot="title">
+                    <Icon type="ios-list"></Icon>
+                    环境列表
+                </p>
+                <Row type="flex" justify="center" align="middle" class="advanced-router">
+                    <Table :columns="shoppingColumns" :data="shoppingData" style="width: 100%;"></Table>
+                </Row>
+            </Card>
+        </Row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'argument-page',
+    data () {
+        return {
+            shoppingColumns: [
+                {
+                    type: 'index',
+                    title: '序号',
+                    width: 60
+                },
+                {
+                    title: '环境名称',
+                    key: 'name',
+                    align: 'center'
+                },
+                {
+                    title: '编辑人',
+                    key: 'edit_user',
+                    align: 'center'
+                },
+                {
+                    title: '编辑时间',
+                    key: 'time'
+                },
+                {
+                    title: '操作',
+                    key: 'option',
+                    align: 'center',
+                    render: (h, params) => {
+                        return h('Button', {
+                            props: {
+                                type: 'text',
+                                size: 'small'
+                            },
+                            on: {
+                                click: () => {
+                                    let query = {name: params.row.name};
+                                    this.$router.push({
+                                        name: 'name',
+                                        query: query
+                                    });
+                                }
+                            }
+                        }, '编辑');
+                    }
+                }
+            ],
+            shoppingData: [
+                {
+                    edit_user: 100001,
+                    name: 'ERP测试环境新库',
+                    time: '2017年11月12日'
+                },
+                {
+                    edit_user: 100002,
+                    name: 'ERP测试环境老库',
+                    time: '2017年11月5日'
+                },
+                {
+                    edit_user: 100003,
+                    name: 'SCP测试环境',
+                    time: '2017年11月8日'
+                },
+                {
+                    edit_user: 100004,
+                    name: 'SCP巡检环境',
+                    time: '2017年11月9日'
+                }
+            ]
+        };
+    }
+};
+</script>
---
+++ src/main/web/src/views/task/task-trigger/task-trigger.vue
@@ -0,0 +1,109 @@
+
+<template>
+    <div>
+        <Row>
+            <Col span="24">
+                <Card>
+                    <p slot="title">
+                        <Icon type="ios-list"></Icon>
+                        触发任务列表
+                    </p>
+                    <Row type="flex" justify="center" align="middle" >
+                        <Table :columns="taskColumns" :data="taskData" style="width: 100%;"></Table>
+                    </Row>
+                </Card>
+            </Col>
+        </Row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'task-trigger',
+    data () {
+        return {
+            taskColumns: [
+                {
+                    type: 'index',
+                    title: '序号',
+                    width: 60
+                },
+                {
+                    title: '任务ID',
+                    key: 'task_id',
+                    align: 'center'
+                },
+                {
+                    title: '状态',
+                    key: 'status',
+                    render: (h, params) => {
+                        const row = params.row;
+                        const color = row.status === 1 ? 'blue' : row.status === 2 ? 'green' : 'red';
+                        const text = row.status === 1 ? '构建中' : row.status === 2 ? '构建完成' : '构建失败';
+
+                        return h('Tag', {
+                            props: {
+                                type: 'dot',
+                                color: color
+                            }
+                        }, text);
+                    }
+                },
+                {
+                    title: '用户',
+                    key: 'user_name'
+                },
+                {
+                    title: '详情',
+                    key: 'show_more',
+                    align: 'center',
+                    render: (h, params) => {
+                        return h('Button', {
+                            props: {
+                                type: 'text',
+                                size: 'small'
+                            },
+                            on: {
+                                click: () => {
+                                    let argu = { order_id: params.row.order_id };
+                                    this.$router.push({
+                                        name: 'order-info',
+                                        params: argu
+                                    });
+                                }
+                            }
+                        }, '查看详情');
+                    }
+                }
+            ],
+            taskData: [
+                {
+                    task_id: '1000001',
+                    user_name: 'Aresn',
+                    status:Math.floor(Math.random() * 3 + 1)
+                },
+                {
+                    task_id: '1000002',
+                    user_name: 'Lison',
+                    status:Math.floor(Math.random() * 3 + 1)
+                },
+                {
+                    task_id: '1000003',
+                    user_name: 'lili',
+                    status:Math.floor(Math.random() * 3 + 1)
+                },
+                {
+                    task_id: '1000004',
+                    user_name: 'lala',
+                    status:Math.floor(Math.random() * 3 + 1)
+                }
+            ]
+        };
+    },
+    computed: {
+        avatorImage () {
+            return localStorage.avatorImgPath;
+        }
+    }
+};
+</script>
